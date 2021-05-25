d4b8f7ab5eb5578d4677e953493108a0ebb23f16
Siddhant Mittal <sidmit2001@gmail.com>
2021-05-25 19:53:16 +0530

    Setting up Apollo Client/Linking with Hasura and Setting up Frontend with Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache
+} from '@apollo/client';
 import './App.css';
+import { Box } from '@chakra-ui/react';
+import { LocalForageWrapper, persistCache } from 'apollo3-cache-persist';
+import ColorCardContainer from './components/ColorCardContainer';
+
+const ADMIN_SECRET_KEY =
+  'biNp3K5fkGAdjXyzw3Klp715Gd2eBRvbg6BVaX33SV5dkMX0FIldjVj4F67LdQWG';
 
 function App() {
-  return <div className="App">HELLO</div>;
+  const cache = new InMemoryCache();
+  persistCache({
+    cache,
+    storage: new LocalForageWrapper(window.localStorage)
+  });
+
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://colorpicker.hasura.app/v1/graphql',
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': ADMIN_SECRET_KEY
+      }
+    }),
+    cache,
+    connectToDevTools: true
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Box minHeight="100%" bgGradient="linear(to-t, blue.800, blue.900)">
+        <Box padding="6">
+          <ColorCardContainer />
+        </Box>
+      </Box>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ src/components/ColorCardContainer.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/layout';
+import React from 'react';
+
+const ColorCardContainer = () => {
+  return (
+    <Box bgColor="white" margin="8" minHeight="90vh" borderRadius="md">
+      COLOR PICKER
+    </Box>
+  );
+};
+
+export default ColorCardContainer;
